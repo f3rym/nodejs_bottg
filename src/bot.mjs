78b2092145489b12c59cd6849dabc645acc4443c
Mihail Ferym <drakomishel@gmail.com>
2024-12-24 23:16:37 +0300

    fix

--- src/bot.mjs
+++ src/bot.mjs
@@ -1,13 +1,20 @@
 import TeleBot from 'telebot';
-import { enemyFind, box1Attack, box2Attack, defAttack } from './enemy.mjs';
+import dotenv from 'dotenv';
+import { enemyFind, box1Attack, box2Attack, defAttack } from './enemy.mjs'; // Подключение функций
+
+dotenv.config();
+
 const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN);
 
 const characters = {};
+const enemy = {}; // Объект для хранения информации о врагах для каждого игрока
 
+// Команда /start
 bot.on('/start', async (msg) => {
     await bot.sendMessage(msg.chat.id, 'Привет, друг, жми /create_character имя класс');
 });
 
+// Команда /create_character
 bot.on(/^\/create_character (.+) (.+)$/, async (msg, props) => {
     const playerId = msg.from.id;
     const name = props.match[1];
@@ -25,29 +32,37 @@ bot.on(/^\/create_character (.+) (.+)$/, async (msg, props) => {
     await bot.sendMessage(msg.chat.id, `Создан персонаж ${name} класса ${charClass}.`);
 });
 
+// Команда /move
 bot.on('/move', async (msg) => {
     const playerId = msg.from.id;
     if (characters[playerId]) {
         characters[playerId].position += 1;
         const position = characters[playerId].position;
 
-        
-        await bot.sendMessage(msg.chat.id, `Вы передвинулись на ${position}. `);
-        if(position % 2 != 0) 
-           enemyFind(msg, characters, enemy);
+        await bot.sendMessage(msg.chat.id, `Вы передвинулись на ${position}.`);
+        if (position % 2 !== 0) {
+            await enemyFind(msg, characters, enemy); // Используем await для асинхронного вызова и передаем объект enemy
+        }
     } else {
         await bot.sendMessage(msg.chat.id, 'Сначала создайте персонажа с помощью команды /create_character имя класс.');
     }
 });
 
+// Команда /box1
 bot.on('/box1', async (msg) => { 
     await box1Attack(msg, characters, enemy); // Используем await для асинхронного вызова
-     }); 
-      bot.on('/box2', async (msg) => {
-         await box2Attack(msg, characters, enemy);
-        }); 
-        bot.on('/fight', async (msg) => {
-             await fightAttack(msg, characters, enemy); 
-        });
+});
+
+// Команда /box2
+bot.on('/box2', async (msg) => {
+    await box2Attack(msg, characters, enemy); // Используем await для асинхронного вызова
+});
+
+// Команда /fight
+bot.on('/fight', async (msg) => {
+    await defAttack(msg, characters, enemy); // Используем await для асинхронного вызова
+});
+
+bot.start();
 
 export default bot;
