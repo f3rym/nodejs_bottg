fe49a3b0287bbf76d5f99003b9b181babea6c42c
Mihail Ferym <drakomishel@gmail.com>
2024-12-25 12:35:40 +0300

    fix

--- src/bot.mjs
+++ src/bot.mjs
@@ -1,5 +1,5 @@
 import TeleBot from 'telebot';
-import { connectDB } from './path/to/db.mjs';
+import { connectDB } from './db.mjs';
 import { enemyFind, box1Attack, box2Attack, defAttack } from './enemy.mjs';
 
 const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
